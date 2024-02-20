c5ad94767556a5c1e12499fd4db352c0d0901a46
DiegolCR <diego200305@gmail.com>
2024-02-20 13:24:38 -0600

    ENTIDADES CATEGORY

---
+++ Northwind/FrondEnd/ApiModels/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace FrondEnd.ApiModels
+{
+    public class Category
+    {
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public string? Description { get; set; }
+
+    }
+}
